adf957e061463f74e9a99b5e5c0031049c5497fe
Taiwo Akerele <taiwoakerele98@gmail.com>
2025-02-04 09:24:37 +0100

    fix: improved input validation

--- index.js
+++ index.js
@@ -28,14 +28,14 @@ const isPerfect = (number) => {
         if (number % i === 0) {
             sum += i;
             if (i !== number / i) sum += number / i;
-        }
-    }
+        };
+    };
     return number > 1 && sum === number;
 };
 
 const getFunFact = async (number) => {
     try {
-        const response = await axios.get(`http://numbersapi.com/${number}/math`);
+        const response = await axios.get(`http://numbersapi.com/${number}/math`)
         return response.data;
     } catch (error) {
         return 'No fun fact available.';
@@ -43,23 +43,28 @@ const getFunFact = async (number) => {
 };
 
 app.get('/api/classify-number', async (req, res) => {
-    const number = parseInt(req.query.number);
 
-    if (isNaN(number)) {
-        return res.status(400).json({ number: req.query.number, error: true });
+    const { number } = req.query;
+
+    if (/[a-zA-Z]/.test(number)) {
+        return res.status(400).json({ number: req.query.number, error: true});
+    };
+
+    if (!/^[-]?\d+$/.test(number)) { 
+        return res.status(400).json({ number: 'Invalid input. Only valid integers are allowed.', error: true });
     }
 
-    const prime = isPrime(number);
-    const armstrong = isArmstrong(number);
-    const perfect = isPerfect(number);
-    const digitSum = number.toString().split('').reduce((acc, digit) => acc + parseInt(digit), 0);
-    const properties = [];
-    if (armstrong) properties.push('armstrong');
-    properties.push(number % 2 === 0 ? 'even' : 'odd');
-    const funFact = await getFunFact(number);
+    const parsedNumber = parseInt(number);
+
+    const prime = isPrime(parsedNumber);
+    const armstrong = isArmstrong(parsedNumber);
+    const perfect = isPerfect(parsedNumber);
+    const digitSum = Math.abs(parsedNumber).toString().split('').reduce((acc, digit) => acc + parseInt(digit), 0);
+    const properties = [armstrong ? 'armstrong': '', parsedNumber % 2 === 0 ? 'even' : 'odd'].filter(Boolean);
+    const funFact = await getFunFact(parsedNumber);
 
     res.status(200).json({
-        number,
+        parsedNumber,
         is_prime: prime,
         is_perfect: perfect,
         properties,
@@ -69,7 +74,7 @@ app.get('/api/classify-number', async (req, res) => {
 });
 
 app.use((req, res) => {
-    res.status(404).json({ error: 'Not Found', message: 'The requested resource does not exist.' });
+    res.status(404).json({ error: 'Not Found', message: 'The requested resource does not exist.'});
 });
 
 app.listen(port, () => {
